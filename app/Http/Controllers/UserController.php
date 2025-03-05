c6edad7ed434a3d6703f6d17ce894943345fe968
Ekya1411 <ekyamuhammad@gmail.com>
2025-03-05 09:35:26 +0700

    try : fillable (js4p1)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -9,9 +9,13 @@ class UserController extends Controller
 {
     public function index () {
         $data = [
-            'nama' => 'Pelanggan Pertama',
+            'level_id' => 2,
+            'username' => 'manager_aslinya_tiga',
+            'nama' => 'Manager aslinya 3',
+            'password' => Hash::make('12345')
         ];
-        UserModel::where('username', 'customer-1')->update($data);
+        UserModel::create($data);
+
         $user = UserModel::all();
         return view('user', ['data' => $user]);
     }
