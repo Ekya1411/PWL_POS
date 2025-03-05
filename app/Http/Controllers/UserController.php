023df7024b05dbf52e7a901b3c1515f83ba9f170
Ekya1411 <ekyamuhammad@gmail.com>
2025-03-05 10:56:11 +0700

    try : or, where, fail methods (js3p2.2)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -8,18 +8,7 @@ use Illuminate\Support\Facades\Hash;
 class UserController extends Controller
 {
     public function index () {
-        // $data = [
-        //     'level_id' => 2,
-        //     'username' => 'manager_aslinya_tiga',
-        //     'nama' => 'Manager aslinya 3',
-        //     'password' => Hash::make('12345')
-        // ];
-        // UserModel::create($data);
-
-
-        $user = UserModel::findOr(20, ['username', 'nama'], function(){
-            abort(404);
-        });
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
     }
 }
