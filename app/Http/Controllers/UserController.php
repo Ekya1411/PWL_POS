2d41f2d0517b0eff0c42dd0aae5b1c3e3cae7a67
Ekya1411 <ekyamuhammad@gmail.com>
2025-03-06 21:41:24 +0700

    try : checking atribut changes (jsp4p2.5)

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -10,13 +10,34 @@ class UserController extends Controller
     public function index () {
         $user = UserModel::firstOrNew(
             [
-                'username' => 'manager',
-                'nama' => 'Manager',
+                'username' => 'manager11',
+                'nama' => 'Manager11',
                 'password' => Hash::make('12345'),
                 'level_id' => 2
             ]
         );
+        $user->username = 'manager12';
+
+        $user->isDirty();
+        $user->isDirty('username');
+        $user->isDirty('nama');
+        $user->isDirty('password');
+        $user->isDirty('nama', 'password');
+
+        $user->isClean();
+        $user->isClean('username');
+        $user->isClean('nama');
+        $user->isClean('password');
+        $user->isClean('nama', 'password');
+
         $user->save();
+
+        $user->wasChanged();
+        $user->wasChanged('username');
+        $user->wasChanged(['username', 'level_id']);
+        $user->wasChanged('name');
+        $user->wasChanged(['nama', 'username']);
+        dd($user->wasChanged(['nama', 'username']));
         return view('user', ['data' => $user]);
     }
 }
